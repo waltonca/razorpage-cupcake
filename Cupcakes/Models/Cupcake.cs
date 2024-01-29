b0970cb142ab53a2204b87c3b38afbdc2d0f3097
Junxiao <W0486229@campus.nscc.ca>
2024-01-28 20:56:39 -0400

    Add Modles folder and Cupcake.cs class file

---
+++ Cupcakes/Models/Cupcake.cs
@@ -0,0 +1,14 @@
+﻿namespace Cupcakes.Models
+{
+    //
+    // This is the Cupcake model, meant to store data in the cupcake table in my database.
+    //
+    public class Cupcake
+    {
+        public int CupcakeId { get; set; }
+        public string Name { get;set; } = string.Empty;
+        public string ImageFileName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
