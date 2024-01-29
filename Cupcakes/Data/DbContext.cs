3ae563ca278596705c395738497564d6275a4b86
Junxiao <W0486229@campus.nscc.ca>
2024-01-28 23:54:22 -0400

    Update Delete page and Add remove cupcake function

--- Cupcakes/Data/DbContext.cs
+++ Cupcakes/Data/DbContext.cs
@@ -8,6 +8,43 @@ namespace Cupcakes.Data
     //
     public static class DbContext
     {
+        // Get a cupcake by its id
+        public static Cupcake GetCupcakeById(int cupcakeId) 
+        {
+            Cupcake cupcake = new Cupcake();
+            // Create connection to database
+            SqliteConnection connection = new SqliteConnection("Data Source=Data/cupcakes.db");
+
+            // Open the connection
+            connection.Open();
+
+            // Run SQL to get a cupcake by its id
+            string sql = "SELECT CupcakeId, Name, ImageFileName, Description, Price FROM Cupcake WHERE CupcakeId = @CupcakeId";
+
+            // Create a "command" (sql to execute in database) to execute SQL
+            SqliteCommand cmd = connection.CreateCommand();
+            cmd.CommandText = sql;
+
+            // Basically do a find and replace in the sql string
+            cmd.Parameters.AddWithValue("@CupcakeId", cupcakeId);
+
+            // Execute the command (use a Data Reader so we can read() the rows returned by the command)
+            SqliteDataReader reader = cmd.ExecuteReader();
+
+            // Read through the result (if any)
+            reader.Read();
+            cupcake.CupcakeId = reader.GetInt32(0);
+            cupcake.Name = reader.GetString(1);
+            cupcake.ImageFileName = reader.GetString(2);
+            cupcake.Description = reader.GetString(3);
+            cupcake.Price = reader.GetDecimal(4);
+
+            // Close the connection
+            connection.Close();
+
+            return cupcake;
+
+        }
         // Get all cupcakes from the database
         public static List<Cupcake> GetAllCupcakes()
         {
@@ -82,5 +119,32 @@ namespace Cupcakes.Data
             // Close the connection
             connection.Close();
         }   
+        
+        // Remove a cupcake from the database
+        public static void RemoveCupcake(int cupcakeId)
+        {
+            // Create connection to database
+            SqliteConnection connection = new SqliteConnection("Data Source=Data/cupcakes.db");
+            
+            // Open the connection
+            connection.Open();
+
+            // Create Sql command to remove cupcake
+            // Use parameters to prevent SQL injection attacks
+            string sql = "DELETE FROM Cupcake WHERE CupcakeId = @CupcakeId";
+
+            // Create command object to execute the query
+            SqliteCommand cmd = connection.CreateCommand();
+            cmd.CommandText = sql;
+
+            // Add parameters to the command
+            cmd.Parameters.AddWithValue("@CupcakeId", cupcakeId);
+
+            // Execute the query
+            cmd.ExecuteNonQuery();
+
+            // Close the connection
+            connection.Close();
+        }
     }
 }
--- Cupcakes/Pages/Delete.cshtml.cs
+++ Cupcakes/Pages/Delete.cshtml.cs
@@ -1,3 +1,5 @@
+using Cupcakes.Data;
+using Cupcakes.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,19 @@ namespace Cupcakes.Pages
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Cupcake Cupcake { get; set; } = new();
+
+        public void OnGet(int id)
         {
+            // Initialize the Cupcake 
+            Cupcake = DbContext.GetCupcakeById(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            DbContext.RemoveCupcake(Cupcake.CupcakeId);
+            return RedirectToPage("/Index");
         }
     }
 }
