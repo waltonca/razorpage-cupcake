5d411c154f29c82f5df18d1439f5e8961573865f
Junxiao <W0486229@campus.nscc.ca>
2024-01-28 21:54:56 -0400

    Update Index page to display all cupcakes name

--- Cupcakes/Pages/Index.cshtml.cs
+++ Cupcakes/Pages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using Cupcakes.Data;
+using Cupcakes.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,14 +9,22 @@ namespace Cupcakes.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public List<Cupcake> cupcakes = new List<Cupcake>();
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+
+            cupcakes = DbContext.GetAllCupcakes();
         }
 
         public void OnGet()
         {
+            
 
+            foreach (Cupcake cupcake in cupcakes)
+            {
+                _logger.Log(LogLevel.Information, cupcake.Name);
+            }
         }
     }
 }
